c9f343209b63319845c64c52432e8755b9d2e140
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-09-17 02:22:13 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -1,15 +1,27 @@
 INPUT_SCHEMA = {
-    "articles": {
+    "prompt": {
         'datatype': 'STRING',
         'required': True,
-        'shape': [-1],
-        'example': [
-            "Diagnosis and Management of Central Diabetes Insipidus in Adults",
-            "Central diabetes insipidus (CDI) is a clinical syndrome which results from loss or impaired function of vasopressinergic neurons in the hypothalamus/posterior pituitary, resulting in impaired synthesis and/or secretion of arginine vasopressin (AVP). [...]",
-            "Adipsic diabetes insipidus",
-            "Adipsic diabetes insipidus (ADI) is a rare but devastating disorder of water balance with significant associated morbidity and mortality. Most patients develop the disease as a result of hypothalamic destruction from a variety of underlying etiologies. [...]",
-            "Nephrogenic diabetes insipidus: a comprehensive overview",
-            "Nephrogenic diabetes insipidus (NDI) is characterized by the inability to concentrate urine that results in polyuria and polydipsia, despite having normal or elevated plasma concentrations of arginine vasopressin (AVP). [...]"
-      ]
-    }
+        'shape': [1],
+        'example': ["Face of a yellow cat, high resolution, sitting on a park bench"]
+    },
+    "image_url": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["https://raw.githubusercontent.com/CompVis/latent-diffusion/main/data/inpainting_examples/overture-creations-5sI6fQgYIuo.png"]
+    },
+    "control_url": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["https://raw.githubusercontent.com/CompVis/latent-diffusion/main/data/inpainting_examples/overture-creations-5sI6fQgYIuo.png"]
+    },
+    "mask_url": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["https://raw.githubusercontent.com/CompVis/latent-diffusion/main/data/inpainting_examples/overture-creations-5sI6fQgYIuo_mask.png"]
+    },
+    
 }
